0c749a069523d49f068cf1905076397573745815
Matt Fraser <matt@mediafraze.com>
2018-01-27 21:15:02 -0800

    added bat

---
+++ release/mediaswitcher v0.0.1/start.bat
@@ -0,0 +1,12 @@
+:: -d is devices
+:: --f is fallback threads
+:: -u1 is supernova username
+:: -w1 is worker for x16r
+:: -p1 is password for worker on x16r
+:: -es is equihash server
+:: -ep is equihash port
+:: -eu is equihash user/address (default is mph)
+:: -ew is equihash worker
+:: -epw is equihash worker password
+
+mediaswitcher.exe -d 0,1,2,3 -f 1 -u1 mediafraze -w1 12x1060 -p1 x -es us-east.equihash-hub.miningpoolhub.com -ep 17023 -eu mediafraze -ew mediaswitcher -epw x
